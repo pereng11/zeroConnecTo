287cafd40f8ebedbf42c3b440cd621d0e56f1c6f
Jaeyeong son <nardi0302@gmail.com>
2022-07-25 16:54:45 +0900

    PostCSS 환경설정

---
+++ Practices/Postcss/postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	plugins: {
+		// 'cssnano': {},
+		'postcss-pxtorem': {},
+		'autoprefixer': {},
+		'postcss-combine-media-query': {},
+		'postcss-preset-env': {
+			stage: 0,
+		},
+		'postcss-import': {},
+	},
+};
